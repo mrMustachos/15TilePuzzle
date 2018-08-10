fe3d4d89a222a7019dc42d38fd3ed8291bc44cf1
mrMustachos <bill.domanick@crowdsurge.com>
2018-08-10 10:00:21 -0400

    saving to try v3

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,4 +1,5 @@
-import React from 'react';
+// import React from 'react';
+import React, { Component } from 'react';
 import classNames from 'classnames';
 import sizeMe from 'react-sizeme';
 import _ from 'lodash';
@@ -6,37 +7,122 @@ import styles from '../assets/sass/Example.scss';
 
 import Tile from './ExampleTile';
 
-const Example = ({ dimension, margin, size, windowSize, user }) => {
-	console.log(size)
-	const containerWidth = windowSize.height;
-	const tileHeight = (size.width - 15) / dimension;
+class Example extends Component {
+	constructor() {
+    super();
+    this.state = {
+      containerWidth: undefined,
+      tileHeight: undefined,
+      maxWidth: undefined,
+    }
+  }
 
-	const setContainerWidth = {
-		maxWidth: `${containerWidth}px`,
-	};
+  componentDidMount() {
+		this.buildArr();
+  }
 
+  // componentDidUpdate() {}
 
-	const renderTiles = () => {
-		const tester = new Array((dimension * dimension) - 1);
-		return _.map(tester, (x, i) => (
-			<Tile
-				tileHeight={ tileHeight }
-				key={ i }
-				info={ i + 1 }
-				user={ user }
-				size={ size }
-			/>
-		));
+  buildArr() {
+  	const { dimension, margin, windowSize, size: { width }, user: { imgUrl, name, profile }} = this.props;
+  	const containerWidth = windowSize.height;
+		const tileHeight = (width - (margin * (dimension - 1))) / dimension;
+
+		let result = [];
+		let total = dimension * dimension - 1;
+		const mogMaker = (i) => i % dimension;		
+
+		for (let i = 1; i <= total; i++) {
+			const left_offset = mogMaker(i) !== 0 ? mogMaker(i) - 1 : dimension - 1;
+			const top_offest = i <= 4 ? 0 : i <= 8 ? 1 : i <= 12 ? 2 : 3;
+			const boardSize = width - margin;
+			const obj = {
+				image: {
+					imgUrl,
+					width: boardSize,
+					height: boardSize,
+					left: -(tileHeight * left_offset),
+					top: -(tileHeight * top_offest),
+				},
+				tile: {
+					height: tileHeight,
+					fontSize: tileHeight * .5,
+				},
+				user: { name, profile },
+			};
+			result.push(obj);
+		}
+		this.setState({ board: result });
+		// return result;
+  }
+
+	render() {
+		return <div>xxx</div>
 	}
-
-	return (
-		<div
-			className={ classNames(styles.row, styles.wafflePuzzle) }
-			style={ setContainerWidth }
-		>
-			{ renderTiles() }
-		</div>
-	);
 }
+// const Example = ({ dimension, margin, windowSize, size: { width, height }, user: { imgUrl, name, profile }}) => {
+
+// const Example = ({ dimension, margin, windowSize, size, user: { imgUrl, name, profile }}) => {
+// 	const containerWidth = windowSize.height;
+// 	const tileHeight = (size.width - (margin * (dimension - 1))) / dimension;
+
+// 	const setContainerWidth = {
+// 		maxWidth: `${containerWidth}px`,
+// 	};
+
+// 	const buildArr = () => {
+// 		let result = [];
+// 		let total = dimension * dimension - 1;
+// 		const mogMaker = (i) => i % dimension;		
+
+// 		for (let i = 1; i <= total; i++) {
+// 			const left_offset = mogMaker(i) !== 0 ? mogMaker(i) - 1 : dimension - 1;
+// 			const top_offest = i <= 4 ? 0 : i <= 8 ? 1 : i <= 12 ? 2 : 3;
+// 			const boardSize = size.width - margin;
+// 			const obj = {
+// 				image: {
+// 					imgUrl,
+// 					width: boardSize,
+// 					height: boardSize,
+// 					left: -(tileHeight * left_offset),
+// 					top: -(tileHeight * top_offest),
+// 				},
+// 				tile: {
+// 					height: tileHeight,
+// 					fontSize: tileHeight * .5,
+// 				},
+// 				user: { name, profile },
+// 			};
+// 			result.push(obj);
+// 		}
+// 		return result;
+// 	}
+
+// 	buildArr()
+
+// 	const renderTiles = () => {
+// 		// const tester = new Array((dimension * dimension) - 1);
+// 		const tester = buildArr();
+// 		return _.map(tester, (x, i) => (
+// 			<Tile
+// 				tileHeight={ tileHeight }
+// 				key={ i }
+// 				info={ i + 1 }
+// 				imgUrl={ imgUrl }
+// 				size={ size }
+// 				pooper={ x }
+// 			/>
+// 		));
+// 	}
+
+// 	return (
+// 		<div
+// 			className={ classNames(styles.row, styles.wafflePuzzle) }
+// 			style={ setContainerWidth }
+// 		>
+// 			{ renderTiles() }
+// 		</div>
+// 	);
+// }
  
 export default sizeMe({ monitorHeight: true })(Example);
--- src/components/ExampleTile.js
+++ src/components/ExampleTile.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import styles from '../assets/sass/Example.scss';
 
-const ExampleTile = ({ info, tileHeight, user, size: { height, width } }) => {
-	const left_offset = info % 4 !== 0 ? info % 4 : 4;
+const ExampleTile = ({ info, tileHeight, imgUrl, size: { height, width } }) => {
+	const left_offset = info % 4 !== 0 ? (info % 4) - 1 : 3;
 	const top_offest = info <= 4 ? 0 : info <= 8 ? 1 : info <= 12 ? 2 : info <= 16 ? 3 : undefined;
 
 	const imgStyling = {
 		width: `${width - 5}px`,
 		height: `${height - 5}px`,
-		left: `${-((tileHeight * left_offset) - tileHeight)}px`,
+		// left: `${-((tileHeight * left_offset) - tileHeight)}px`,
+		left: `${-(tileHeight * left_offset)}px`,
 		top: `${-(tileHeight * top_offest)}px`,
 	};
 
@@ -17,7 +18,7 @@ const ExampleTile = ({ info, tileHeight, user, size: { height, width } }) => {
 
 	return (
 		<div className={ styles.tile } style={ tileStyling }>
-			<img src={ user.imgUrl } alt={`Tile ${info}`} style={ imgStyling }/>
+			<img src={ imgUrl } alt={`Tile ${info}`} style={ imgStyling }/>
 			<span style={ spanStyling }>{ info }</span>
 		</div>
 	);
